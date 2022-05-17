2a3dfa1235eca849273edfc88f616fd6799cd40c
Heming Chen <hemingchenhc@gmail.com>
2022-05-16 22:42:46 -0400

    Add python support in lsp

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -63,6 +63,7 @@ return packer.startup(function(use)
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
   use "doums/darcula"
+  use "shaunsingh/nord.nvim"
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
