f1ebf4fef8d1ec5cb8fee520b57ae856e1db4ccf
Heming Chen <hemingchenhc@gmail.com>
2022-05-24 14:44:47 -0400

    Adjust plugin load sequence

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -65,6 +65,9 @@ return packer.startup(function(use)
   use "hemingchen/darcula"
   use "shaunsingh/nord.nvim"
 
+  -- Illumate
+  use "RRethy/vim-illuminate"
+
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
   use "hrsh7th/cmp-buffer" -- buffer completions
@@ -82,9 +85,6 @@ return packer.startup(function(use)
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
-
-  -- Illumate
-  use "RRethy/vim-illuminate"
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
